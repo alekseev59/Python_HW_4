6cb9095931ab4cc3012cc8a1103b04a5dccfdac8
Nikita <alekseev.nikitaa@gmail.com>
2022-09-14 04:23:45 +0500

    добавил Task004

---
+++ Task004.py
@@ -0,0 +1,42 @@
+# Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# Пример:
+# k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+import random
+
+
+def rnd():
+    return random.randint(0, 101)
+
+
+def mn(k):
+    lst = [rnd() for i in range(k+1)]
+    return lst
+
+
+def str(sp):
+    lst = sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
+
+
+k = int(input("Введите натуральную степень k = "))
+k1 = mn(k)
+print(f'Список коэффициентов многочлена заданной степени k = {(str(k1))}')
